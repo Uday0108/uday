71a11798121bc539e3dc3c59aef0c0c5fb74b3e0
Uday0108 <121805628+Uday0108@users.noreply.github.com>
2024-06-27 14:21:49 +0530

    Update newscript.js

--- newscript.js
+++ newscript.js
@@ -1,3 +1,6 @@
-function event(){
-
-}
+var typed  = new Typed(".auto-type",{
+    strings : ["programmer...","designer...","developer..."],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+})
